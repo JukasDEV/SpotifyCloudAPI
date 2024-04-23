b4f589e0f1824f8f682a2ec1b0bf2c00ae515458
Joao Lucas Curvello <103194851+JukasDEV@users.noreply.github.com>
2024-04-23 16:53:05 -0300

    first commit

---
+++ SpotifyAPI/src/main/java/com/ProjetoCloud/SpotifyAPI/SpotifyApiApplication.java
@@ -0,0 +1,15 @@
+package com.ProjetoCloud.SpotifyAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpotifyApiApplication {
+
+	public static void main(String[] args) {
+		System.out.println("FUNCIONA PORRA");
+		SpringApplication.run(SpotifyApiApplication.class, args);
+
+		System.out.println("FUNCIONA PORRA2");
+	};
+}
